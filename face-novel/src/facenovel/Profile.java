709f567f0ddbf423afadb68d349a2a39c0850955
p3nny10 <nguyenxa3356@gmail.com>
2020-04-30 14:03:33 -0700

    Profile class updated

--- face-novel/src/facenovel/Profile.java
+++ face-novel/src/facenovel/Profile.java
@@ -27,14 +27,14 @@ public class Profile {
 		@return  The profile's picture. */
 	public BufferedImage getProfilePicture()
 	{
-		
+		return picture;
 	} // end getProfilePicture
 
 	/** Sets the picture associated with the profile to the given picture.
 		@param newPicture  The new picture associated with the profile. */
 	public void setProfilePicture(BufferedImage newPicture)
 	{
-		
+		picture = newPicture;
 	} // end setProfilePicture
 
 	/** Sets the name associated with the profile to the given name.
@@ -42,42 +42,42 @@ public class Profile {
        @param lastName   The last name for the profile. */
    public void setName(String firstName, String lastName)
    {
-      
+      name = firstName + " " + lastName;
    } // end setName
 
    /** Returns the name associated with the profile.
        @return  The profile's name. */
    public String getName()
    {
-      
+      return name;
    } // end getName
    
 	/** Sets the current status of the profile to the given string.
 		@param stat  The new status for the profile. */
 	public void setStatus(String stat)
 	{
-		
+		status = stat;
 	} // end setStatus
 
 	/** Returns the status associated with the profile.
 		@return  The profile's status.*/
 	public String getStatus()
 	{
-		
+		return status;
 	} // end getStatus
 
 	/** Returns a list of all the person's friendProfiles on the social network.
 		@return  The list of friendProfiles. */
 	public ArrayList<Profile> getFriends()
 	{
-		
+		return friendProfiles;
 	} // end getFriends
 
 	/** Adds a friend to the profile's list of friendProfiles.
 		@param p  The profile to be added to the list. */
 	public void addFriend(Profile p)
 	{
-		
+		friendProfiles.add(p);
 	} // end addFriend
 
 	/** Removes a friend from the profile's list of friendProfiles.
@@ -85,17 +85,17 @@ public class Profile {
        @return  True if the profile was removed. */
 	public boolean removeFriend(Profile p)
 	{
-		
+		return friendProfiles.remove(p);
 	} // end removeFriend
 
 	public String toString()
 	{
-		
+		return "toString";
 	} // end toString
 
 	/** Displays the profile's information and friendProfiles. */
 	public void display()
 	{
-	
+		System.out.println();
 	} // end display
 } // end Profile
