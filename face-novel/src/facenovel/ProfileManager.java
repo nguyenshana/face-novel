45d2b6f8e5e85361d73cc300fe0feb484955a2ea
p3nny10 <nguyenxa3356@gmail.com>
2020-05-08 16:11:19 -0700

    current code

--- face-novel/src/facenovel/ProfileManager.java
+++ face-novel/src/facenovel/ProfileManager.java
@@ -1,44 +1,53 @@
 package facenovel;
 import java.awt.image.*;
 import java.util.*;
-//import GraphPackage.*;
-//import ADTPackage.*;
-/**
-	An implementation of a profile manager on a simple social network.
-
-	@author Jesse Grabowski
-	@author Joseph Erickson
-	@version 5.0 */
 
 public class ProfileManager
 {
-	private UndirectedGraph<Profile> allProfiles;
+    private UndirectedGraphs<Profile> allProfiles;
 
-	/** Constructor for an instance of a profile manager. */
-	public ProfileManager()
-	{
-		allProfiles = new UndirectedGraph<>();
-	} // end default constructor
+    public ProfileManager()
+    {
+        allProfiles = new UndirectedGraphs<>();
+    } // end default constructor
 
-	/** Adds a profile to the social network.
-		@param p  The profile to be added to the network. */
-	public void addProfile(Profile p)
-	{
-		allProfiles.addVertex(p);
-	} // end addProfile
 
-	/** Creates a friendship between two users on the social network.
-		@param a  The first profile in the friendship.
-		@param b  The second profile in the friendship. */
-	public void createFriendship(Profile a, Profile b)
-	{
-		a.addFriend(b);
-		b.addFriend(a);
-	} // end createFriendship
+    public void addProfile(Profile p)
+    {
+        allProfiles.addVertex(p);
+    } // end addProfile
 
-	/** Displays each profile's information and friends. */
-	public void display(Profile startPoint)
-	{
-		startPoint.display();
-	}
+    public void createFriendship(Profile a, Profile b)
+    {
+        a.addFriend(b);
+        b.addFriend(a);
+    } // end createFriendship
+
+    public void leaveNetwork(Profile person)
+    {
+        allProfiles.remove(person);
+    }
+
+    public Profile findPerson(String searchName)
+    {
+        for(Profile person : allProfiles.getNetwork()) 
+        {
+            if(person.getName().equals(searchName)) 
+            {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    /** Displays each profile's information and friends. */
+    public void display(Profile startPoint)
+    {
+        ArrayList<Profile> network = allProfiles.getNetwork();
+        int startIndex = network.indexOf(startPoint);
+        for(int i = startIndex; i < network.size(); i++)
+        {
+            network.get(i).display();
+        }
+    }
 } // end ProfileManager
