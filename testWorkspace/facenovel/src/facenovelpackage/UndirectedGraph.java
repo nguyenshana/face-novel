99dfe1951178ffbdc6c4838ea56a2e6c99f252e4
p3nny10 <64501464+p3nny10@users.noreply.github.com>
2020-05-09 21:01:25 -0700

    Update UndirectedGraph.java

--- testWorkspace/facenovel/src/facenovelpackage/UndirectedGraph.java
+++ testWorkspace/facenovel/src/facenovelpackage/UndirectedGraph.java
@@ -1,28 +1,33 @@
 package facenovelpackage;
-
 import java.util.*;
 
 public class UndirectedGraph<T extends Profile>
 {
 
 	private ArrayList<T> network;
-
+	
+	/** Constructor for an instance of an undirected graph. */
 	public UndirectedGraph()
 	{
 		network = new ArrayList<T>();
 	}
 
-	// add a person to the network
+	/** Adds an object/vertex to the graph.
+	@param vertex  The vertex to be added to the network. */
 	public void addVertex(T vertex)
 	{
 		network.add(vertex);
 	}
 
+	/** Returns the network.
+	@return the network of the graph */
 	public ArrayList<T> getNetwork()
 	{
 		return network;
 	}
 	
+	/** Removes an object/vertex from the graph.
+	@param vertex  The vertex to be removed from the network. */
 	public void remove(T vertex)
 	{
 		Profile toRemove = (Profile) vertex;
@@ -36,7 +41,4 @@ public class UndirectedGraph<T extends Profile>
 		}
 		network.remove(toRemove);
 	}
-
-	// add remove
-
 }
